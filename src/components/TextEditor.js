dfa3d014ab5a3e61af69496fe23b09ad57b8a38b
byio <ben@benyang.io>
2018-07-18 16:45:35 +0800

    create TextEditor component; import and use in src/pages/index.js

---
+++ src/components/TextEditor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TextEditor extends Component {
+  render () {
+    return (
+      <div>TextEditor component</div>
+    );
+  }
+}
+
+export default TextEditor;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+import TextEditor from '../components/TextEditor';
+
+export default () => <TextEditor />
