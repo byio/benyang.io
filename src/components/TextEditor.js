b8327eaef02aa53b92ccd2bbeafd471f022edab4
byio <ben@benyang.io>
2018-07-18 17:16:10 +0800

    install, import, and use Typist component from 'react-typist' in TextEditor component

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
 
 class TextEditor extends Component {
   render () {
     return (
-      <div>TextEditor component</div>
+      <div>
+        <Typist>hello world</Typist>
+      </div>
     );
   }
 }
