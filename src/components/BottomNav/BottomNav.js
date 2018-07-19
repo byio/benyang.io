5f1b7e3191f9daac2aca3fc894486843abf0d2ee
byio <ben@benyang.io>
2018-07-19 18:47:14 +0800

    render BottomNav, created from material-ui components, in src/pages/index.js

--- src/components/BottomNav/BottomNav.js
+++ src/components/BottomNav/BottomNav.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import WorkIcon from '@material-ui/icons/Work';
+import AboutIcon from '@material-ui/icons/PersonPin';
+import ContactIcon from '@material-ui/icons/RingVolume';
 
 import styles from './BottomNav.module.css';
 
 class BottomNav extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      value: 'home'
+    };
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render () {
+    const { value } = this.state;
+
     return (
-      <div className={styles.main}>
-        BottomNav Component
-      </div>
+      <BottomNavigation
+        value={ value }
+        onChange={this.handleChange}
+      >
+        <BottomNavigationAction
+          label="Home"
+          value="home"
+          icon={<HomeIcon />}
+        />
+        <BottomNavigationAction
+          label="Works"
+          value="works"
+          icon={<WorkIcon />}
+        />
+        <BottomNavigationAction
+          label="About"
+          value="about"
+          icon={<AboutIcon />}
+        />
+        <BottomNavigationAction
+          label="Contact"
+          value="contact"
+          icon={<ContactIcon />}
+        />
+      </BottomNavigation>
     );
   }
 }
