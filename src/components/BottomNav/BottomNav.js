f15712cd024d93f65d0c7ff7c3e4c141d698d75f
byio <ben@benyang.io>
2018-07-19 16:14:14 +0800

    create BottomNav component and accompanying module.css file; import into src/pages/index.js

---
+++ src/components/BottomNav/BottomNav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import styles from './BottomNav.module.css';
+
+class BottomNav extends Component {
+  render () {
+    return (
+      <div className={styles.main}>
+        BottomNav Component
+      </div>
+    );
+  }
+}
+
+export default BottomNav;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import TextEditor from '../components/TextEditor/TextEditor';
 import GooeyNav from '../components/GooeyNav/GooeyNav';
+import BottomNav from '../components/BottomNav/BottomNav';
 import intro from '../assets/intro.js';
 
 export default () => (
   <div>
     <TextEditor text={intro} />
     <GooeyNav />
+    <BottomNav />
   </div>
 );
