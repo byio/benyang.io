628d5e0cd8588b0763060b6dfbba99f3f74f1285
byio <ben@benyang.io>
2018-07-23 11:19:35 +0800

    create works.js, which is an array of objects with information of each project

---
+++ src/assets/works.js
@@ -0,0 +1,52 @@
+export const works = [
+  {
+    title: 'PWA Newsfeed',
+    imgURL: 'https://picsum.photos/200/?random',
+    description: 'Read articles from a wide range of international sources.
+    This PWA Newsfeed app is built using ReactJS, following PWA standards
+    recommended by Google (https://developers.google.com/web/ilt/pwa),
+    and deployed on Google\'s Firebase.',
+    githubURL: 'https://github.com/byio/pwa-newsfeed',
+    liveURL: 'https://pwa-newsfeed-4f116.firebaseapp.com/',
+  },
+  {
+    title: 'PWA Newsfeed',
+    imgURL: 'https://picsum.photos/200/?random',
+    description: 'Read articles from a wide range of international sources.
+    This PWA Newsfeed app is built using ReactJS, following PWA standards
+    recommended by Google (https://developers.google.com/web/ilt/pwa),
+    and deployed on Google\'s Firebase.',
+    githubURL: 'https://github.com/byio/pwa-newsfeed',
+    liveURL: 'https://pwa-newsfeed-4f116.firebaseapp.com/',
+  },
+  {
+    title: 'PWA Newsfeed',
+    imgURL: 'https://picsum.photos/200/?random',
+    description: 'Read articles from a wide range of international sources.
+    This PWA Newsfeed app is built using ReactJS, following PWA standards
+    recommended by Google (https://developers.google.com/web/ilt/pwa),
+    and deployed on Google\'s Firebase.',
+    githubURL: 'https://github.com/byio/pwa-newsfeed',
+    liveURL: 'https://pwa-newsfeed-4f116.firebaseapp.com/',
+  },
+  {
+    title: 'PWA Newsfeed',
+    imgURL: 'https://picsum.photos/200/?random',
+    description: 'Read articles from a wide range of international sources.
+    This PWA Newsfeed app is built using ReactJS, following PWA standards
+    recommended by Google (https://developers.google.com/web/ilt/pwa),
+    and deployed on Google\'s Firebase.',
+    githubURL: 'https://github.com/byio/pwa-newsfeed',
+    liveURL: 'https://pwa-newsfeed-4f116.firebaseapp.com/',
+  },
+  {
+    title: 'PWA Newsfeed',
+    imgURL: 'https://picsum.photos/200/?random',
+    description: 'Read articles from a wide range of international sources.
+    This PWA Newsfeed app is built using ReactJS, following PWA standards
+    recommended by Google (https://developers.google.com/web/ilt/pwa),
+    and deployed on Google\'s Firebase.',
+    githubURL: 'https://github.com/byio/pwa-newsfeed',
+    liveURL: 'https://pwa-newsfeed-4f116.firebaseapp.com/',
+  }
+];
