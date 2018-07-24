ef84c7151dac6a73ee4e7cac678cb4431dd5a004
byio <ben@benyang.io>
2018-07-24 13:57:55 +0800

    return CardList component from Portfolio container component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import SearchBox from '../SearchBox/SearchBox';
+import CardList from '../CardList/CardList'
+import { works } from '../../assets/works';
 
 class Portfolio extends Component {
   render () {
     return (
       <div>
         <SearchBox />
+        <CardList works={works} />
       </div>
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,12 @@ import BottomNav from '../components/BottomNav/BottomNav';
 import CardList from '../components/CardList/CardList';
 import Portfolio from '../components/Portfolio/Portfolio';
 import intro from '../assets/intro.js';
-import { works } from '../assets/works';
 
 export default () => (
   <div>
     <TextEditor text={intro} />
     <GooeyNav />
     <Portfolio />
-    <CardList works={works} />
     <BottomNav />
   </div>
 );
