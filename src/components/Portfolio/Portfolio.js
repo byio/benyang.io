35885b915ecde8fcac6b22c216a7d9ada8aa852b
byio <ben@benyang.io>
2018-07-24 13:46:01 +0800

    create Portfolio container component that, and return SearchBox component; render Portfolio component in src/pages/index.js instead of SearchBox component

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import SearchBox from '../SearchBox/SearchBox';
+
+class Portfolio extends Component {
+  render () {
+    return (
+      <div>
+        <SearchBox />
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import TextEditor from '../components/TextEditor/TextEditor';
 import GooeyNav from '../components/GooeyNav/GooeyNav';
 import BottomNav from '../components/BottomNav/BottomNav';
 import CardList from '../components/CardList/CardList';
-import SearchBox from '../components/SearchBox/SearchBox';
+import Portfolio from '../components/Portfolio/Portfolio';
 import intro from '../assets/intro.js';
 import { works } from '../assets/works';
 
@@ -12,7 +12,7 @@ export default () => (
   <div>
     <TextEditor text={intro} />
     <GooeyNav />
-    <SearchBox />
+    <Portfolio />
     <CardList works={works} />
     <BottomNav />
   </div>
