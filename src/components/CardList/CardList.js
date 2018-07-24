9976f9417c4a107235f2cde77a19dc4d83594014
byio <ben@benyang.io>
2018-07-24 08:43:50 +0800

    pass works array into CardList component as props, and map array, returning a Card component for each project in the works array

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 
 import Card from '../Card/Card';
 
-const CardList = () => {
+const CardList = ({ works }) => {
+  const renderCards = works.map((work, i) => {
+    return (
+      <Card
+        imgURL={works[i].imgURL}
+        title={works[i].title}
+        description={works[i].description}
+        githubURL={works[i].githubURL}
+        liveURL={works[i].liveURL}
+      />
+    );
+  });
+
   return (
     <div>
-      <Card />
+      {renderCards}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import GooeyNav from '../components/GooeyNav/GooeyNav';
 import BottomNav from '../components/BottomNav/BottomNav';
 import CardList from '../components/CardList/CardList';
 import intro from '../assets/intro.js';
+import { works } from '../assets/works';
 
 export default () => (
   <div>
     <TextEditor text={intro} />
     <GooeyNav />
-    <CardList />
+    <CardList works={works} />
     <BottomNav />
   </div>
 );
