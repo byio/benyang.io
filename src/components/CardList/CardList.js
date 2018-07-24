2624dcfd59eb505d40e30befffd769170d46e594
byio <ben@benyang.io>
2018-07-24 08:06:05 +0800

    create CardList component which renders a Card component

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Card from '../Card/Card';
+
+const CardList = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default CardList;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import TextEditor from '../components/TextEditor/TextEditor';
 import GooeyNav from '../components/GooeyNav/GooeyNav';
 import BottomNav from '../components/BottomNav/BottomNav';
-import Card from '../components/Card/Card';
+import CardList from '../components/CardList/CardList';
 import intro from '../assets/intro.js';
 
 export default () => (
   <div>
     <TextEditor text={intro} />
     <GooeyNav />
-    <Card />
+    <CardList />
     <BottomNav />
   </div>
 );
