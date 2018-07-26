e5e8a7ff508a99c65542c7533d7b29223c3c40d0
byio <ben@benyang.io>
2018-07-26 14:39:57 +0800

    create about page boilerplate

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>About page</h1>
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import TextEditor from '../components/TextEditor/TextEditor';
-import GooeyNav from '../components/GooeyNav/GooeyNav';
 import BottomNav from '../components/BottomNav/BottomNav';
 import CardList from '../components/CardList/CardList';
 import Portfolio from '../components/Portfolio/Portfolio';
@@ -10,7 +9,6 @@ import intro from '../assets/intro.js';
 export default () => (
   <div>
     <TextEditor text={intro} />
-    <GooeyNav />
     <Portfolio />
     <BottomNav />
   </div>
